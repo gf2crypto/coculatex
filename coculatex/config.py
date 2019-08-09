d67336684c19ed9a84ba7638297c58b07838c94b
Chizhov Ivan <ivchizhov@gmail.com>
2019-08-09 13:46:10 +0300

    Renamed the directory and the main script file

---
+++ coculatex/config.py
@@ -0,0 +1,98 @@
+"""
+    Configuration variables for ALL modules
+"""
+import os
+import logging
+from collections import namedtuple
+
+
+LOG = logging.getLogger(__name__)
+
+# ------------JINJA2 Config----------------------------------------------------
+
+Jinja2Config = namedtuple(
+    'jinja2',
+    ('BLOCK_START_STRING BLOCK_END_STRING VARIABLE_START_STRING '
+     'VARIABLE_END_STRING COMMENT_START_STRING COMMENT_END_STRING '
+     'LINE_STATEMENT_PREFIX LINE_COMMENT_PREFIX TRIM_BLOCKS AUTOESCAPE'))
+
+
+JINJA2_DEFAULT_CONFIG = Jinja2Config(
+    BLOCK_START_STRING=r'\BLOCK{',
+    BLOCK_END_STRING='}',
+    VARIABLE_START_STRING=r'\VAR{',
+    VARIABLE_END_STRING='}',
+    COMMENT_START_STRING=r'\#{',
+    COMMENT_END_STRING='}',
+    LINE_STATEMENT_PREFIX=r'%%',
+    LINE_COMMENT_PREFIX=r'%#',
+    TRIM_BLOCKS=True,
+    AUTOESCAPE=False)
+
+
+# ------------LaTeX Template Config--------------------------------------------
+
+LatexTemplateConfig = namedtuple(
+    'latex_template',
+    'LINE_STATEMENT_PREFIX')
+
+
+LATEX_TEMPLATE_DEFAULT_CONFIG = LatexTemplateConfig(
+    LINE_STATEMENT_PREFIX=r'%%=')
+
+YAML_LINE_PREFIX = r'%%='
+# -----------------------------------------------------------------------------
+
+THEMES_DIRECTORY = os.path.realpath(os.path.expanduser('~/latextm_themes'))
+
+THEME_CONFIG_FILE_NAME = 'config.yaml'
+
+THEME_SUBTHEMES = 'subthemes'
+
+OUTPUT_DIR = ''
+
+DEFAULT_ROOT_TEX = 'root.tex'
+
+LTM_FILE_EXTENSION = 'ltm'
+
+VERSION_STRING_TEMPLATE = "CoCuLaTeX {version} by Ivan Chizhov"
+
+
+def config_iter(config):
+    """
+        Iteration over configuration values
+    """
+    for attr_name, attr_value in _namedtuple_iter(config):
+        yield attr_name.lower(), attr_value
+
+
+def _namedtuple_iter(named_tuple):
+    """
+        Iteration over attributes and values of namedtuple object
+    """
+    for attr_name in named_tuple._fields:
+        yield attr_name, getattr(named_tuple, attr_name)
+
+
+def set_user_config(config, user_config):
+    """
+        Function set user configuration
+        config - `namedtuple` config instance
+        user_config - `dict` with user config
+    """
+    LOG.debug('Changing configuration: %r', config)
+    user_config = user_config.get(config.__class__.__name__, {})
+    LOG.debug('Using user configuration: %s', user_config)
+    new_config = {}
+    for attr_name, attr_value in _namedtuple_iter(config):
+        LOG.debug('Processing attribute `%s` (default value = %s)',
+                  attr_name, attr_value)
+        value = user_config.get(attr_name.lower())
+        LOG.debug('User value of attribute `%s`: %s', attr_name, value)
+        new_config[attr_name] = (value
+                                 if isinstance(value, type(attr_value))
+                                 else attr_value)
+        LOG.debug('New value of attribute `%s`: %s',
+                  attr_name, new_config[attr_name])
+    LOG.debug('Final configuration: %s', new_config)
+    return config.__class__(**new_config)
---
+++ coculatex/templates.py
@@ -0,0 +1,57 @@
+"""Module contains functions for working with various templates."""
+import logging
+import os
+import yaml
+from jinja2 import BaseLoader, TemplateNotFound
+from coculatex import config
+
+
+LOG = logging.getLogger(__name__)
+
+
+class TemplateLoader(BaseLoader):
+    """Redefined BaseLoader to specify path where search the templates."""
+
+    def __init__(self, path):
+        """Init the Loader by using path variabel."""
+        self.path = path
+        LOG.debug('Define the TemplateLoader for the path %s', self.path)
+
+    def get_source(self, environment, template):
+        """Get the source file for the template."""
+        path = os.path.join(self.path, template)
+        if not os.path.exists(path):
+            raise TemplateNotFound(template)
+        mtime = os.path.getmtime(path)
+        with open(path, encoding='utf-8') as file:
+            source = file.read()
+        return source, path, lambda: mtime == os.path.getmtime(path)
+
+
+def extract_variables(file):
+    """
+    Extract variables from templates.
+
+    :file: - file object or any similar (iterable) object
+    :return:
+        `dict` variables, - the dictionary that contains variables
+        `str` clear_template - file lines cleared of variables
+    """
+    LOG.debug("Extract variables from iterables")
+    prefix = config.YAML_LINE_PREFIX
+    LOG.debug("Prefix of line which is contains varibales: %s", prefix)
+    len_prefix = len(prefix)
+    var_strings = ''
+    cleared_template = ''
+    for line in file:
+        if len(line) >= len_prefix and line[:len_prefix] == prefix:
+            var_strings += line[len_prefix:]
+        else:
+            cleared_template += line
+    try:
+        values = yaml.safe_load(var_strings)
+    except yaml.scanner.ScannerError as error:
+        LOG.debug('The problem happens while load values from '
+                  'the string %s: %s', var_strings, error)
+        values = {}
+    return values, cleared_template
