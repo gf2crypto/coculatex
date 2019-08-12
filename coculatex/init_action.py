bc59d6cd8ce44e130b7c246273f2e27f93be3d1f
Chizhov Ivan <ivchizhov@gmail.com>
2019-08-12 16:56:06 +0300

    Added init action module

---
+++ coculatex/init_action.py
@@ -0,0 +1,94 @@
+"""The module containing the function to action of the `init` command."""
+import logging
+from os import (getcwd,
+                path,
+                makedirs)
+from yaml import dump
+from coculatex.config import LTCONFIG
+from coculatex.themeloader import load_theme
+
+LOG = logging.getLogger(__name__)
+
+
+def register(arg_parser):
+    """Register the parameters of the argument parser."""
+    parser = arg_parser.add_parser(
+        'init',
+        description=('Create the config file from the theme. '
+                     'You can it customise for your project.'))
+    parser.add_argument('--project-name', '--name', '-n',
+                        type=str, action='store',
+                        help=('it defines the name of your project'))
+    parser.add_argument('--output-path', '--output-directory',
+                        '--out-path', '--out-dir', '-o',
+                        default=getcwd(),
+                        type=str, action='store',
+                        help=('the path to the output directory'))
+    parser.add_argument('--embed', '-e', action='store_true',
+                        default=False,
+                        help=('place the values to the tex-files, '
+                              'don\'t use the separate yaml-config file'
+                              )
+                        )
+    parser.add_argument('theme', action='store',
+                        type=str, help=('the name of the theme'))
+    parser.set_defaults(func=handler)
+
+
+def handler(theme,
+            project_name=None,
+            output_directory=None,
+            embed=False):
+    """Init the theme `theme` in the output directory.
+
+    In the result of the function evaluation,
+    the sample config file will be created.
+
+    :param: `str` theme - the name of the theme
+    :param: `str` project_name - the name of the project
+    :param: `str` output_directory - the path to output directory
+    :param: `bool` embed - if it is true the function places config
+                           straight to the tex-file
+    :return: no return value
+    """
+    if not project_name:
+        project_name = theme
+    LOG.debug('The project name is %s', project_name)
+    LOG.debug('The output directory is %s', output_directory)
+    theme_config = load_theme(theme)
+    LOG.debug('The theme `%s` from is loaded: %s',
+              theme, theme_config)
+    theme_parameters = {'theme': theme, 'project-name': project_name}
+    theme_parameters.update(theme_config.get('parameters', {}))
+    theme_parameters.update({'tex-preambule': '',
+                             'tex-options': {}})
+    config_dump = dump(theme_parameters,
+                       sort_keys=False,
+                       allow_unicode=True)
+    if embed:
+        output_file = path.join(output_directory,
+                                project_name + '.source.tex')
+        content = ''
+        for line in config_dump.split('\n'):
+            if line:
+                content += '{} {}\n'.format(
+                    LTCONFIG['config_prefix'],
+                    line)
+            else:
+                content += '\n'
+    else:
+        output_file = path.join(output_directory,
+                                project_name + '.yaml')
+        content = config_dump
+    LOG.debug('The output file: %s', output_file)
+    LOG.debug('The content of the theme configuration:\n%s', content)
+    makedirs(output_directory, exist_ok=True)
+    try:
+        with open(output_file, 'w', encoding='utf-8') as file:
+            file.write(content)
+    except (IOError, PermissionError) as error:
+        LOG.error('Sorry! I cannot write the output file %s. '
+                  'Please, check the correctness and the '
+                  'existence of the path and '
+                  'the permissions it.\n'
+                  'The error: %s', output_file, error)
