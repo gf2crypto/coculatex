3ec9312af4dfc19caccfe976e5aaf43610ae289b
Ivan Chizhov <i.chizhov@kryptonite.ru>
2019-08-14 12:15:32 +0300

    Add new variables which is consider of the theme's parameters

--- coculatex/config.py
+++ coculatex/config.py
@@ -66,6 +66,29 @@ SECTION_NAMES_CONFIG = {
                                       # the configuration of jinja2 template
 }
 
+# Additiona theme variables which is specified in user configuration
+PARAMETERS_NAMES_CONFIG = {
+    'theme': 'theme',  # the theme's name block
+    'project-name': 'project-name',  # the project name's block
+    'tex-preambule': 'tex-preambule',  # the block containing the tex preambule
+    'tex-options': 'tex-options',       # the block containing
+                                        # the tex additional options
+    'tex-sources': 'tex-sources'  # the list of sources file block
+}
+
+# This parameters will add in the begin of the configuration
+PARAMETERS_BEGIN = [
+    PARAMETERS_NAMES_CONFIG['theme'],
+    PARAMETERS_NAMES_CONFIG['project-name']
+]
+
+# This parameters will add in the end of the configuration
+PARAMETERS_BEGIN = [
+    PARAMETERS_NAMES_CONFIG['tex-preambule'],
+    PARAMETERS_NAMES_CONFIG['tex-options'],
+    PARAMETERS_NAMES_CONFIG['tex-sources']
+]
+
 # The theme's configuration
 
 THEME_CONFIG = {
@@ -79,6 +102,15 @@ THEME_CONFIG = {
     SECTION_NAMES_CONFIG['readme']: '',  # the readme file name
     SECTION_NAMES_CONFIG['example']: '',  # the relative path to the example
     SECTION_NAMES_CONFIG['jinja2_config']: {}  # the jinja2's configuration
+}
+
+# The theme's parameters section configuration
+THEME_PARAMETERS_CONFIG = {
+    PARAMETERS_NAMES_CONFIG['theme']: '',
+    PARAMETERS_NAMES_CONFIG['project-name']: '',
+    PARAMETERS_NAMES_CONFIG['tex-preambule']: '',
+    PARAMETERS_NAMES_CONFIG['tex-options']: {},
+    PARAMETERS_NAMES_CONFIG['tex-sources']: []
 }
 
 # The list of the registered themes
