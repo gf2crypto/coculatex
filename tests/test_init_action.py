7ce5bd1c77f8955e938f3f39740a7cf51279c87c
Ivan Chizhov <ivchizhov@gmail.com>
2019-09-25 12:07:36 +0300

    Add test case for the example creating function

--- tests/test_init_action.py
+++ tests/test_init_action.py
@@ -149,5 +149,269 @@ class ThemeInitTestCase(unittest.TestCase):
             self.assertEqual(file.read(), content)
 
 
+class ThemeInitMakeExampleTestCase(unittest.TestCase):
+    """Test Case for init theme and make the example sources."""
+
+    def setUp(self):
+        """Prepartion for the test case."""
+        self.tempdir = TemporaryDirectory()
+        self.out_dir = TemporaryDirectory()
+        config.THEMES_PATH = self.tempdir.name
+
+        self.themes_config = {
+            'alpha_one_source': (
+                'path: {path}\n'
+                '{subthemes}:\n'
+                '    a: a/a.yaml\n'
+                '    b: b.yaml\n'
+                '{parameters}:\n'
+                '    param1: test1\n'
+                '    param2: test2\n'
+                '{description}: is alpha theme for the test\n'
+                '{root_file}: alpha.tex\n'
+                '{tex_program}: pdflatex\n'
+                '{tex_options}:\n'
+                '    - option1\n'
+                '    - option2\n'
+                '    - option3\n'
+                '{include_files}:\n'
+                '    file1.tex: template_file1.tex\n'
+                '    file2.tex: template_file2.tex\n'
+                '{readme}: readme.txt\n'
+                '{example}:\n'
+                '    path: examples/alpha\n'
+                '    sources:\n'
+                '        - section01.tex\n'
+                '{jinja2_config}:\n'
+                '    autoescape: true\n'
+                '    line_comment_prefix:' r' "%%##"' '\n'
+                ''.format(**config.SECTION_NAMES_CONFIG,
+                          path=path.join(self.tempdir.name, 'alpha',
+                                         'config.yaml'))
+            ),
+            'alpha_multiple_source': (
+                'path: {path}\n'
+                '{subthemes}:\n'
+                '    a: a/a.yaml\n'
+                '    b: b.yaml\n'
+                '{parameters}:\n'
+                '    param1: test1\n'
+                '    param2: test2\n'
+                '{description}: is alpha theme for the test\n'
+                '{root_file}: alpha.tex\n'
+                '{tex_program}: pdflatex\n'
+                '{tex_options}:\n'
+                '    - option1\n'
+                '    - option2\n'
+                '    - option3\n'
+                '{include_files}:\n'
+                '    file1.tex: template_file1.tex\n'
+                '    file2.tex: template_file2.tex\n'
+                '{readme}: readme.txt\n'
+                '{example}:\n'
+                '    path: examples/alpha\n'
+                '    sources:\n'
+                '        - section01.tex\n'
+                '        - section02.tex\n'
+                '{jinja2_config}:\n'
+                '    autoescape: true\n'
+                '    line_comment_prefix:' r' "%%##"' '\n'
+                ''.format(**config.SECTION_NAMES_CONFIG,
+                          path=path.join(self.tempdir.name, 'alpha',
+                                         'config.yaml'))
+            ),
+        }
+        self.examples = {
+            'section01.tex': (
+                '\\section{{This is the first section}}\n'
+                'Hello, I\'m first section!\n'
+                'This is just text: bla, bla, bla\n'
+                ),
+            'section02.tex': (
+                '\\section{{This is the second section}}\n'
+                'Hello, I\'m second section!\n'
+                'The text of the section number 2.\n'
+                'Third line of the section\'s text\n'
+                ),
+            'pictures/pic.jpg': (
+                'This is picture\n.'
+                'Sorry, It is text picture.'
+                ),
+            'mystyle.sty': (
+                '\\newcommand{{\\mycommand}}{{1}}{{\\textbf{{#1}}}}\n'
+                )
+        }
+
+    def make_directories_tree(self, one_source=True):
+        """Make the directories tree."""
+        if one_source:
+            name = 'alpha_one_source'
+        else:
+            name = 'alpha_multiple_source'
+        theme = safe_load(StringIO(self.themes_config[name]))
+        path_theme = path.dirname(theme['path'])
+        makedirs(path.dirname(path_theme), exist_ok=True)
+        with open(theme['path'], 'w') as file:
+            file.write(self.themes_config[name])
+        dir_example = path.normpath(
+            theme[config.SECTION_NAMES_CONFIG['example']])
+        path_example = path.join(path_theme, dir_example)
+        for file_name, source in self.examples.items():
+            path_file = path.join(path_example,
+                                  path.normpath(file_name))
+            makedirs(path.dirname(path_file), exist_ok=True)
+            with open(path_file, 'w') as file:
+                file.write(source)
+
+    def tearDown(self):
+        """Clean the system after tests."""
+        self.tempdir.cleanup()
+        self.out_dir.cleanup()
+
+    def test_init_example_one_source_embed(self):
+        """Test init the theme and make example, embeded configuration.
+
+        Example has one source file.
+        """
+        self.make_directories_tree()
+        handler('alpha', project_name='my_project',
+                output_directory=self.out_dir.name,
+                embed=True, make_example=True)
+        out_path = path.join(self.out_dir.name,
+                             'my_project' + '.{}'.format(
+                                 config.LTCONFIG['source_ext']))
+        self.assertTrue(path.exists(out_path))
+        params = {'theme': 'alpha', 'project-name': 'my_project'}
+        theme = safe_load(StringIO(self.themes_config['alpha_one_source']))
+        params.update(theme.get('parameters', {}))
+        params.update({
+            'tex-options': [],
+            'tex-preambule': '',
+            'tex-sources': []})
+        content = ''
+        for line in safe_dump(params, sort_keys=False).split('\n'):
+            if line:
+                content += '{} {}\n'.format(
+                    config.LTCONFIG['config_prefix'],
+                    line)
+            else:
+                content += '\n'
+        content += '\n'
+        source_name = theme[
+            config.SECTION_NAMES_CONFIG['example']]['sources'][0]
+        content += self.examples[source_name]
+        with open(out_path, 'r', encoding='utf-8') as file:
+            self.assertEqual(file.read(), content)
+        for file_name, source in self.examples.items():
+            if file_name == source_name:
+                continue
+            file_path = path.join(self.out_dir.name,
+                                  path.normpath(file_name))
+            self.assertTrue(path.exists(file_path))
+            with open(file_path, 'r') as file:
+                self.assertEqual(file.read(), source)
+
+    def test_init_example_multiple_source_embed(self):
+        """Test init the theme and make example, embeded configuration.
+
+        Example has multiple sources file.
+        """
+        self.make_directories_tree(one_source=False)
+        handler('alpha', project_name='my_project',
+                output_directory=self.out_dir.name,
+                embed=True, make_example=True)
+        out_path = path.join(self.out_dir.name,
+                             'my_project' + '.{}'.format(
+                                 config.LTCONFIG['source_ext']))
+        self.assertTrue(path.exists(out_path))
+        params = {'theme': 'alpha', 'project-name': 'my_project'}
+        theme = safe_load(StringIO(self.themes_config['alpha_multiple_source']))
+        params.update(theme.get('parameters', {}))
+        params.update({
+            'tex-options': [],
+            'tex-preambule': '',
+            'tex-sources': theme.get(
+                config.SECTION_NAMES_CONFIG['example'],
+                {}).get('sources', [])})
+        content = ''
+        for line in safe_dump(params, sort_keys=False).split('\n'):
+            if line:
+                content += '{} {}\n'.format(
+                    config.LTCONFIG['config_prefix'],
+                    line)
+            else:
+                content += '\n'
+        with open(out_path, 'r', encoding='utf-8') as file:
+            self.assertEqual(file.read(), content)
+        for file_name, source in self.examples.items():
+            file_path = path.join(self.out_dir.name,
+                                  path.normpath(file_name))
+            self.assertTrue(path.exists(file_path))
+            with open(file_path, 'r') as file:
+                self.assertEqual(file.read(), source)
+
+    def test_example_not_embeded_one_source(self):
+        """Test init the theme and make example, not embeded configuration.
+
+        Example has one source file.
+        """
+        self.make_directories_tree()
+        handler('alpha', project_name='my_project',
+                output_directory=self.out_dir.name,
+                embed=True, make_example=True)
+        out_path = path.join(self.out_dir.name,
+                             config.THEME_CONFIG_FILENAME)
+        self.assertTrue(path.exists(out_path))
+        params = {'theme': 'alpha', 'project-name': 'my_project'}
+        theme = safe_load(StringIO(self.themes_config['alpha_one_source']))
+        params.update(theme.get('parameters', {}))
+        params.update({
+            'tex-options': [],
+            'tex-preambule': '',
+            'tex-sources': theme.get(
+                config.SECTION_NAMES_CONFIG['example'],
+                {}).get('sources', [])[0]})
+        content = safe_dump(params, sort_keys=False)
+        with open(out_path, 'r', encoding='utf-8') as file:
+            self.assertEqual(file.read(), content)
+        for file_name, source in self.examples.items():
+            file_path = path.join(self.out_dir.name,
+                                  path.normpath(file_name))
+            self.assertTrue(path.exists(file_path))
+            with open(file_path, 'r') as file:
+                self.assertEqual(file.read(), source)
+
+    def test_example_not_embeded_multiple_source(self):
+        """Test init the theme and make example, not embeded configuration.
+
+        Example has one source file.
+        """
+        self.make_directories_tree()
+        handler('alpha', project_name='my_project',
+                output_directory=self.out_dir.name,
+                embed=True, make_example=True)
+        out_path = path.join(self.out_dir.name,
+                             config.THEME_CONFIG_FILENAME)
+        self.assertTrue(path.exists(out_path))
+        params = {'theme': 'alpha', 'project-name': 'my_project'}
+        theme = safe_load(StringIO(self.themes_config['alpha_one_source']))
+        params.update(theme.get('parameters', {}))
+        params.update({
+            'tex-options': [],
+            'tex-preambule': '',
+            'tex-sources': theme.get(
+                config.SECTION_NAMES_CONFIG['example'],
+                {}).get('sources', [])})
+        content = safe_dump(params, sort_keys=False)
+        with open(out_path, 'r', encoding='utf-8') as file:
+            self.assertEqual(file.read(), content)
+        for file_name, source in self.examples.items():
+            file_path = path.join(self.out_dir.name,
+                                  path.normpath(file_name))
+            self.assertTrue(path.exists(file_path))
+            with open(file_path, 'r') as file:
+                self.assertEqual(file.read(), source)
+
+
 if __name__ == '__main__':
     unittest.main(verbosity=0)
